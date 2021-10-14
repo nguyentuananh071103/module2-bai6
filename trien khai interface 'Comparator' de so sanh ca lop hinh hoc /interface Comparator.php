2c1abac580a330ae40a8c0d34f3d67694f57b907
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2021-10-14 16:52:28 +0700

    update

---
+++ trien khai interface 'Comparator' de so sanh ca lop hinh hoc /interface Comparator.php
@@ -0,0 +1,6 @@
+<?php
+
+interface Comparator
+{
+    function compare($circleOne, $circleTwo);
+}
---
+++ trien khai interface'Comparable' cho cac lop hinh hoc/Circle.php
@@ -0,0 +1,27 @@
+<?php
+class Circle
+{
+    public $radius;
+    public $name;
+    public function __construct($name, $radius)
+    {
+        $this->radius = $radius;
+        $this->name = $this;
+    }
+        public function getName()
+        {
+            return $this->name;
+        }
+        public function setName($name)
+        {
+            $this->name = $name;
+        }
+        public function getRadius()
+        {
+            return $this->radius;
+        }
+        public function setRadius($radius)
+        {
+            $this->radius = $radius;
+        }
+}
